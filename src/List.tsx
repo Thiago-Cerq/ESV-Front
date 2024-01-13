20111846b0cbeaee8f4b37496e3f858790a723d4
carollingian <carolina.fdcampos@gmail.com>
2024-01-12 23:30:42 -0300

    fazendo filtro de busca

--- src/List.tsx
+++ src/List.tsx
@@ -3,6 +3,9 @@ import{ useForm } from 'react-hook-form';
 import { yupResolver } from "@hookform/resolvers/yup"
 import * as yup from "yup"
 import './Navbar.css'
+
+import FilterImage from './assets/filter.png'
+
 'use client;'
 
 const schema = yup
@@ -24,10 +27,32 @@ console.log(errors);
   return (
 
     <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <span>{errors.pesquisa?.message}</span>
-        <input {...register('pesquisa', {required: true})} type="text" placeholder="Pesquise por uma informação" />
-      </form>
+
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <span>{errors.pesquisa?.message}</span>
+          <input {...register('pesquisa', {required: true})} type="text" placeholder="Pesquise por uma informação" 
+          className='pesquisa'/>
+        </form>
+        <img src={FilterImage} className="filtragem"/>
+
+        <div className="dropdown-filter">
+          <h4>Filtro</h4>
+          <hr />
+          <label>UF</label>
+          <select>
+            <option value="">AC</option> <option value="">AL</option> <option value="">AP</option> <option value="">AM</option>
+            <option value="">BA</option> <option value="">CE</option> <option value="">DF</option> <option value="">ES</option>
+            <option value="">GO</option> <option value="">MA</option> <option value="">MT</option> <option value="">MS</option>
+            <option value="">MG</option> <option value="">PA</option> <option value="">PB</option> <option value="">PR</option>
+            <option value="">PE</option> <option value="">PI</option> <option value="">RJ</option> <option value="">RN</option>
+            <option value="">RS</option> <option value="">RO</option> <option value="">RR</option> <option value="">SC</option>
+            <option value="">SP</option> <option value="">SE</option> <option value="">TO</option>
+          </select>
+        </div>
+      </div>
+
+    
 
     <table border="0" width="100%" className="tabela">
       <thead>
