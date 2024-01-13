0bb783c2b627cd96b83fac26db9eb13e13a46b2e
carollingian <carolina.fdcampos@gmail.com>
2024-01-13 17:49:20 -0300

    botoes editar e excluir

--- src/List.tsx
+++ src/List.tsx
@@ -6,8 +6,9 @@ import './Navbar.css'
 
 import React, {useState, useEffect, useRef} from 'react';
 
-import FilterImage from './assets/filter.png'
 import FilterImage1 from './assets/filter1.png'
+import TrashImage from './assets/trash.png'
+import EditImage from './assets/edit.png'
 
 'use client;'
 
@@ -56,7 +57,7 @@ function List() {
 
         <div ref={filterRef}>
         <div className='filter-trigger' onClick={()=>{setOpen(!open)}}>
-          <img src={FilterImage1} className="filtragem" id="changeimg"/>
+          <img src={FilterImage1} className="filtragem"/>
         </div>
         
           <div className={`dropdown-filter ${open? "active" : "inactive"}`}>
@@ -66,6 +67,7 @@ function List() {
               <DropdownItem title = {"Tipo"} option = {"Restaurante"}/>
               <DropdownItem title = {"UF"} option = {"AC"}/>
             </ul>
+            <button type="submit" className='filter-button'> Buscar </button>
           </div>
         </div>
         </div>
@@ -83,12 +85,40 @@ function List() {
         <td>DF</td>
         <td>Brasília</td>
         <td>St. Leste Projeção A - Gama Leste, Brasília - DF, 72444-240</td>
+        <td><button type="submit" className='action-button'> <img src={EditImage} className="action-image"/> </button></td>
+        <td><button type="submit" className='action-button'> <img src={TrashImage} className="action-image"/> </button></td>
       </tbody>
       <tbody>
         <td>Exemplozinho 2</td>
         <td>RJ</td>
         <td>Rio de Janeiro</td>
         <td>Rua Lauro Müller, 116 - Botafogo, Rio de Janeiro - RJ, 22290-160</td>
+        <td><button type="submit" className='action-button'> <img src={EditImage} className="action-image"/> </button></td>
+        <td><button type="submit" className='action-button'> <img src={TrashImage} className="action-image"/> </button></td>
+      </tbody>
+      <tbody>
+        <td>Mega Ultra Exemplo 3</td>
+        <td>SP</td>
+        <td>São Paulo</td>
+        <td>R. da Reitoria, R. Cidade Universitária, 374 - Butantã, São Paulo - SP, 05508-220</td>
+        <td><button type="submit" className='action-button'> <img src={EditImage} className="action-image"/> </button></td>
+        <td><button type="submit" className='action-button'> <img src={TrashImage} className="action-image"/> </button></td>
+      </tbody>
+      <tbody>
+        <td>GIGANTESCO EXEMPLO EXEMPLAR 4</td>
+        <td>SC</td>
+        <td>Florianópolis</td>
+        <td>Eng. Agronômico Andrei Cristian Ferreira, s/n - Trindade, Florianópolis - SC, 88040-900</td>
+        <td><button type="submit" className='action-button'> <img src={EditImage} className="action-image"/> </button></td>
+        <td><button type="submit" className='action-button'> <img src={TrashImage} className="action-image"/> </button></td>
+      </tbody>
+      <tbody>
+        <td>Exemplo 5</td>
+        <td>BA</td>
+        <td>Salvador</td>
+        <td>Av. Milton Santos, s/nº - Ondina, Salvador - BA, 40170-110</td>
+        <td><button type="submit" className='action-button'> <img src={EditImage} className="action-image"/> </button></td>
+        <td><button type="submit" className='action-button'> <img src={TrashImage} className="action-image"/> </button></td>
       </tbody>
       {/* criar um loop aqui após integração */}
     </table>
